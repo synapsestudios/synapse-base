bff6160f41bc8607b5b288ae33e9064d948a8c0d
Paul Statezny <paul@synapsestudios.com>
2014-04-02 09:35:12 -0700

    Move controller resolver to controller namespace.

--- src/Synapse/Controller/ServiceProvider.php
+++ src/Synapse/Controller/ServiceProvider.php
@@ -15,7 +15,7 @@ class ServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $app) {
-            return new ControllerResolver($resolver, $app);
+            return new Resolver($resolver, $app);
         }));
 
         $app->initializer(
