cc5110d9a7241f165e09f209593a8d5c2a27c066
Paul Statezny <paul@synapsestudios.com>
2014-04-03 14:33:32 -0700

    Refs #12 - Add mapper traits for pivot tables.

---
+++ src/Synapse/Mapper/PivotDeleterTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Synapse\Mapper;
+
+use Synapse\Entity\AbstractEntity;
+
+/**
+ * Specialized version of DeleterTrait that does not delete on the ID of an entity,
+ * but rather on all columns, since pivot tables do not have an ID column.
+ *
+ * Use this trait to add delete functionality to AbstractMappers for pivot tables.
+ */
+trait DeleterTrait
+{
+    /**
+     * Delete record corresponding to this entity
+     *
+     * @param  AbstractEntity $entity
+     * @return Result
+     */
+    public function delete(AbstractEntity $entity)
+    {
+        $conditions = $entity->getArrayCopy();
+
+        $query = $this->sql()
+            ->delete()
+            ->where($condition);
+
+        return $this->execute($query);
+    }
+}
---
+++ src/Synapse/Mapper/PivotInserterTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Synapse\Mapper;
+
+use Synapse\Entity\AbstractEntity;
+
+/**
+ * Specialized version of InserterTrait that does not set the ID on an inserted entity,
+ * since pivot tables do not have an ID column.
+ *
+ * Use this trait to add create functionality to AbstractMappers for pivot tables.
+ */
+trait PivotInserterTrait
+{
+    /**
+     * Insert the given entity into the database
+     *
+     * @param  AbstractEntity $entity
+     * @return AbstractEntity         Entity with ID populated
+     */
+    public function insert(AbstractEntity $entity)
+    {
+        $values = $entity->getDbValues();
+
+        $columns = array_keys($values);
+
+        $query = $this->sql()
+            ->insert()
+            ->columns($columns)
+            ->values($values);
+
+        $statement = $this->sql()->prepareStatementForSqlObject($query);
+
+        $result = $statement->execute();
+
+        return $entity;
+    }
+}
