073a65549374cd99a6b82addd107d78939a02af6
Paul Statezny <paul@synapsestudios.com>
2014-04-03 11:36:18 -0700

    Refs #11 - Extract important service provider registrations into register() method.
    Only put firewall definition in protected method.

--- src/Synapse/Application/Services.php
+++ src/Synapse/Application/Services.php
@@ -3,7 +3,6 @@
 namespace Synapse\Application;
 
 use Synapse\Application;
-use Symfony\Component\HttpFoundation\RequestMatcher;
 
 /**
  * Define services
@@ -16,8 +15,14 @@ class Services implements ServicesInterface
      */
     public function register(Application $app)
     {
+        // Register log provider first to catch any exceptions thrown in the others
+        $app->register(new \Synapse\Log\ServiceProvider);
+
+        // Register security component before other providers attempt to extend $app['security.firewalls']
+        $app->register(new \Silex\Provider\SecurityServiceProvider);
+        $this->registerSecurityFirewalls($app);
+
         $this->registerServiceProviders($app);
-        $this->registerSecurity($app);
     }
 
     /**
@@ -27,9 +32,6 @@ class Services implements ServicesInterface
      */
     protected function registerServiceProviders(Application $app)
     {
-        // Register log provider first to catch any exceptions thrown in the others
-        $app->register(new \Synapse\Log\ServiceProvider);
-
         $app->register(new \Synapse\Command\ServiceProvider);
         $app->register(new \Synapse\Db\ServiceProvider);
         $app->register(new \Synapse\OAuth2\ServerServiceProvider);
@@ -60,39 +62,28 @@ class Services implements ServicesInterface
     }
 
     /**
-     * Register the security context
+     * Register the security firewalls for use with the Security Context in SecurityServiceProvider
+     *
+     * How to add application-specific firewalls:
+     *
+     *     $app->extend('security.firewalls, function ($firewalls, $app) {
+     *         $newFirewalls = [...];
+     *
+     *         return array_merge($newFirewalls, $firewalls);
+     *     });
+     *
+     * It's important to return an array with $firewalls at the end, as in the example,
+     * so that the catch-all 'base.api' firewall does not preclude more specific firewalls.
+     *
+     * Application-specific firewalls should only be needed to allow passthrough
+     * for public endpoints, since 'base.api' requires authentication.
      *
      * @param  Application $app
      */
-    public function registerSecurity(Application $app)
+    public function registerSecurityFirewalls(Application $app)
     {
-        $app->register(new \Silex\Provider\SecurityServiceProvider);
-
-        /**
-         * Security firewalls
-         *
-         * How to add application-specific firewalls:
-         *
-         *     $app->extend('security.firewalls, function ($firewalls, $app) {
-         *         $newFirewalls = [...];
-         *
-         *         return array_merge($newFirewalls, $firewalls);
-         *     });
-         *
-         * It's important to return an array with $firewalls at the end, as in the example,
-         * so that the catch-all 'base.api' firewall does not preclude more specific firewalls.
-         */
         $app['security.firewalls'] = $app->share(function () {
-            $createUser = new RequestMatcher('^/users$', null, ['POST']);
-
             return [
-                'base.unsecured' => [
-                    'pattern'   => '^/(oauth|social-login)',
-                ],
-                'base.create-users' => [
-                    'pattern'   => $createUser, // Make user creation endpoint public for user registration
-                    'anonymous' => true,
-                ],
                 'base.api' => [
                     'pattern'   => '^/',
                     // Order of oauth and anonymous matters!
