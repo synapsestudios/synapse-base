059e70c6bd358092bd0f0064830ec1ec7dc985fd
Paul Statezny <paul@synapsestudios.com>
2014-06-12 11:58:08 -0700

    Refs #84 - Test that finder trait returns entity with found data injected.

--- tests/Test/Synapse/Mapper/FinderTraitTest.php
+++ tests/Test/Synapse/Mapper/FinderTraitTest.php
@@ -4,25 +4,42 @@ namespace Test\Synapse\Mapper;
 
 use Synapse\TestHelper\MapperTestCase;
 use Synapse\User\UserEntity;
+use stdClass;
 
 class FinderTraitTest extends MapperTestCase
 {
     const ORDER_ASCENDING  = 'ASC';
     const ORDER_DESCENDING = 'DESC';
 
+    // Set to a non-round number to make the tests stronger
+    const RESULTS_PER_PAGE = 47;
+
     public function setUp()
     {
         parent::setUp();
+
+        $this->captured = new stdClass();
 
         $this->prototype = $this->createPrototype();
 
         $this->mapper = new Mapper($this->mockAdapter, $this->prototype);
         $this->mapper->setSqlFactory($this->mockSqlFactory);
+
+        $this->mapper->setResultsPerPage(self::RESULTS_PER_PAGE);
     }
 
     public function createPrototype()
     {
         return new UserEntity();
+    }
+
+    public function createUserEntity()
+    {
+        return new UserEntity([
+            'id'       => 100,
+            'email'    => 'address@a.com',
+            'password' => 'password',
+        ]);
     }
 
     public function createSearchConstraints()
@@ -33,6 +50,19 @@ class FinderTraitTest extends MapperTestCase
             'qux'  => null,
             'life' => 42,
         ];
+    }
+
+    public function withUserEntityFound()
+    {
+        $userEntity = $this->createUserEntity();
+
+        $this->mockResult->expects($this->any())
+            ->method('current')
+            ->will($this->returnCallback(function () use ($userEntity) {
+                $this->captured->foundUserEntityData = $userEntity->getArrayCopy();
+
+                return $this->captured->foundUserEntityData;
+            }));
     }
 
     public function provideOrderDirectionValues()
@@ -54,6 +84,18 @@ class FinderTraitTest extends MapperTestCase
         $regexp = sprintf('/SELECT `%s`.* FROM `%s`/', $tableName, $tableName);
 
         $this->assertRegExp($regexp, $this->getSqlString());
+    }
+
+    public function testFindByReturnsEntityWithFoundDataInjected()
+    {
+        $this->withUserEntityFound();
+
+        $result = $this->mapper->findBy([]);
+
+        $this->assertSame(
+            $this->captured->foundUserEntityData,
+            $result->getArrayCopy()
+        );
     }
 
     public function testFindByConstructsWhereClausesWithAnds()
@@ -203,7 +245,71 @@ class FinderTraitTest extends MapperTestCase
         $this->assertRegExp($regexp, $this->getSqlString());
     }
 
-    public function testFindAllBySimplySelectsAllRowsFromTable()
+    /**
+     * @expectedException LogicException
+     */
+    public function testFindAllByThrowExceptionIfPageGivenButOrderNotGiven()
+    {
+        $options = ['page'  => 6];
+
+        $this->mapper->findAllBy($this->createSearchConstraints(), $options);
+    }
+
+    public function testFindAllBySetsLimitAndOffsetIfPaginationOptionsProvided()
+    {
+        $page  = 6;
+        $order = 'foo';
+
+        $options = [
+            'page'  => $page,
+            'order' => [$order],
+        ];
+
+        $this->mapper->findAllBy($this->createSearchConstraints(), $options);
+
+        // Results per page set in $this::setUp
+        $limit  = self::RESULTS_PER_PAGE;
+
+        // Offset is the start of the page
+        $offset = ($page - 1) * $limit;
+
+        $regexp = sprintf(
+            '/LIMIT \'%s\' OFFSET \'%s\'/',
+            $limit,
+            $offset
+        );
+
+        $this->assertRegExpOnSqlString($regexp);
+    }
+
+    public function testFindAllBySetsPageTo1IfBelow1()
+    {
+        $page  = -15;
+        $order = 'foo';
+
+        $options = [
+            'page'  => $page,
+            'order' => [$order],
+        ];
+
+        $this->mapper->findAllBy($this->createSearchConstraints(), $options);
+
+        // Results per page set in $this::setUp
+        $limit  = self::RESULTS_PER_PAGE;
+
+        // Offset is the start of the page
+        $offset = ($page - 1) * $limit;
+
+        $regexp = sprintf(
+            '/LIMIT \'%s\' OFFSET \'%s\'/',
+            $limit,
+            $offset
+        );
+
+        $this->assertRegExpOnSqlString($regexp);
+    }
+
+    public function testFindAllSimplySelectsAllRowsFromTable()
     {
         $tableName = $this->mapper->getTableName();
 
