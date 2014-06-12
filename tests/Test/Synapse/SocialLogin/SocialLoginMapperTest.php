3e138b6988fd7ccdd5f1787225dd3c967a5f4f9a
Paul Statezny <paul@synapsestudios.com>
2014-06-12 10:18:11 -0700

    Refs #84 - Test SocialLoginMapper.

---
+++ tests/Test/Synapse/SocialLogin/SocialLoginMapperTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Test\Synapse\SocialLogin;
+
+use Synapse\TestHelper\MapperTestCase;
+use Synapse\SocialLogin\SocialLoginMapper;
+use Synapse\SocialLogin\SocialLoginEntity;
+
+class SocialLoginMapperTest extends MapperTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mapper = new SocialLoginMapper($this->mockAdapter, new SocialLoginEntity);
+        $this->mapper->setSqlFactory($this->mockSqlFactory);
+    }
+
+    public function testFindByUserIdSearchesByUserIdOnly()
+    {
+        $userId = 10294857;
+
+        $this->mapper->findByUserId($userId);
+
+        $regexp = sprintf('/WHERE `user_id` = \'%s\'/', $userId);
+
+        $this->assertRegExp($regexp, $this->getSqlString());
+    }
+
+    public function testFindByProviderUserIdSearchesByProviderAndProviderUserId()
+    {
+        $providerUserId = 1022957;
+        $provider       = 'github';
+
+        $this->mapper->findByProviderUserId($provider, $providerUserId);
+
+        $regexp = sprintf(
+            '/WHERE `provider` = \'%s\' AND `provider_user_id` = \'%s\'/',
+            $provider,
+            $providerUserId
+        );
+
+        $this->assertRegExp($regexp, $this->getSqlString());
+    }
+}
