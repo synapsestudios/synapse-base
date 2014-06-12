d5fad52c79388880dc29a87e0b79f125f14a74b4
Paul Statezny <paul@synapsestudios.com>
2014-06-12 09:13:05 -0700

    Refs #84 - Test deleter trait.

---
+++ tests/Test/Synapse/Mapper/DeleterTraitTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Test\Synapse\Mapper;
+
+use Synapse\TestHelper\MapperTestCase;
+use Synapse\User\UserEntity;
+
+class DeleterTraitTest extends MapperTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->prototype = $this->createPrototype();
+
+        $this->mapper = new Mapper($this->mockAdapter, $this->prototype);
+        $this->mapper->setSqlFactory($this->mockSqlFactory);
+    }
+
+    public function createPrototype()
+    {
+        return new UserEntity();
+    }
+
+    public function createEntityToDelete()
+    {
+        return new UserEntity([
+            'id'         => 12345,
+            'email'      => 'test@example.com',
+            'password'   => 'password',
+            'last_login' => time(),
+            'created'    => time(),
+            'enabled'    => '0',
+            'verified'   => '0',
+        ]);
+    }
+
+    public function createDeletionConstraints()
+    {
+        return [
+            'foo'  => 'bar',
+            'baz'  => 1.99,
+            'qux'  => null,
+            'life' => 42,
+        ];
+    }
+
+    public function testDeleteDeletesFromCorrectTable()
+    {
+        $tableName = $this->mapper->getTableName();
+
+        $entity = $this->createEntityToDelete();
+
+        $this->mapper->delete($entity);
+
+        $regexp = sprintf('/DELETE FROM `%s`/', $tableName);
+
+        $this->assertRegExp($regexp, $this->getSqlString());
+    }
+
+    public function testDeleteDeletesWhereIdIsIdOfEntity()
+    {
+        $entity = $this->createEntityToDelete();
+
+        $regexp = sprintf('/WHERE `id` = \'%s\'/', $entity->getId());
+
+        $this->mapper->delete($entity);
+
+        $this->assertRegExp($regexp, $this->getSqlString());
+    }
+
+    public function testDeleteWhereConstructsWhereClausesCorrectly()
+    {
+        $constraints = $this->createDeletionConstraints();
+
+        $whereValues = [];
+
+        foreach ($constraints as $field => $value) {
+            if ($value === null) {
+                $whereValue = sprintf('`%s` IS NULL', $field);
+            } else {
+                $whereValue = sprintf('`%s` = \'%s\'', $field, $value);
+            }
+
+            $whereValues[] = $whereValue;
+        }
+
+        $whereValueString = implode(' AND ', $whereValues);
+
+        $regexp = sprintf('/WHERE %s/', $whereValueString);
+
+        $this->mapper->deleteWhere($constraints);
+
+        $this->assertRegExp($regexp, $this->getSqlString());
+    }
+}
