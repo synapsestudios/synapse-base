8c4f746a54a56faf57abcf04d77124401ab45d98
tdboone <tdboone@gmail.com>
2014-07-24 15:05:29 -0700

    #96 - added some doc blocks

--- src/Synapse/TestHelper/AbstractSecurityAwareTestCase.php
+++ src/Synapse/TestHelper/AbstractSecurityAwareTestCase.php
@@ -5,9 +5,16 @@ namespace Synapse\TestHelper;
 use PHPUnit_Framework_TestCase;
 use Synapse\User\UserEntity;
 
+/**
+ * Extend this class to create mocks of the security token and context for testing
+ */
 abstract class AbstractSecurityAwareTestCase extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Create the mock security context and the mock security token, and sets up
+     *  the getUser and getToken mock functions
+     */
     public function setUpMockSecurityContext()
     {
         $this->mockSecurityContext = $this->getMockBuilder('Symfony\Component\Security\Core\SecurityContext')
@@ -27,6 +34,10 @@ abstract class AbstractSecurityAwareTestCase extends PHPUnit_Framework_TestCase
             ->will($this->returnValue($mockSecurityToken));
     }
 
+    /**
+     * Return a Mocked UserEntity object
+     * @return UserEntity
+     */
     public function getDefaultLoggedInUserEntity()
     {
         $user = new UserEntity;
