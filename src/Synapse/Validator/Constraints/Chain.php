09ac1d4acca0c214c2d34165c495177edf820c93
Zachary Chavez <zach@synapsestudios.com>
2014-07-24 10:30:29 -0700

    Refs #95 - Break up long lines.

--- src/Synapse/Validator/Constraints/Chain.php
+++ src/Synapse/Validator/Constraints/Chain.php
@@ -22,29 +22,40 @@ class Chain extends Constraint
     public function __construct($options = null)
     {
         // no known options set? $options is the constraints array
-        if (is_array($options) && !array_intersect(array_keys($options), array('groups', 'constraints', 'stopOnError'))) {
-            $options = array('constraints' => $options);
+        if (is_array($options)
+            && !array_intersect(
+                array_keys($options),
+                ['groups', 'constraints', 'stopOnError']
+            )
+        ) {
+            $options = ['constraints' => $options];
         }
 
         parent::__construct($options);
 
         if (!is_array($this->constraints)) {
-            throw new ConstraintDefinitionException('The option "constraints" is expected to be an array in constraint '.__CLASS__.'.');
+            throw new ConstraintDefinitionException(
+                'The option "constraints" is expected to be an array in constraint '.__CLASS__.'.'
+            );
         }
 
         foreach ($this->constraints as $constraint) {
             if (!$constraint instanceof Constraint) {
-                throw new ConstraintDefinitionException('The value '.$constraint.' is not an instance of Constraint in constraint '.__CLASS__.'.');
+                throw new ConstraintDefinitionException(
+                    'The value '.$constraint.' is not an instance of Constraint in constraint '.__CLASS__.'.'
+                );
             }
 
             if ($constraint instanceof Valid) {
-                throw new ConstraintDefinitionException('The constraint Valid cannot be nested inside constraint '.__CLASS__.'.');
+                throw new ConstraintDefinitionException(
+                    'The constraint Valid cannot be nested inside constraint '.__CLASS__.'.'
+                );
             }
         }
     }
 
     public function getRequiredOptions()
     {
-        return array('constraints');
+        return ['constraints'];
     }
 }
