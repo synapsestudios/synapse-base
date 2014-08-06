100e5f85ef927c9d8b1547bf3d7fcc9d3f37281c
Aaron Bruce <aaron@synapsestudios.com>
2014-08-06 15:28:03 -0700

    Refs #67 - adding test for SocialLoginService

---
+++ tests/Test/Synapse/SocialLogin/SocialLoginServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Synapse\SocialLogin;
+
+use Synapse\SocialLogin\SocialLoginService;
+use PHPUnit_Framework_TestCase;
+
+class SocialLoginServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->socialLoginService = new SocialLoginService();
+
+        $this->createMocks();
+
+        $this->socialLoginService->setSocialLoginMapper($this->mockSocialLoginMapper);
+    }
+
+    public function createMocks()
+    {
+        $this->mockSocialLoginMapper = $this->getMockBuilder('Synapse\SocialLogin\SocialLoginMapper')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockSocialLoginMapper->expects($this->any())
+            ->method('persist')
+            ->will($this->returnCallback(function ($entity) {
+                return $entity;
+            }));
+    }
+
+    public function testTokenUpdateOnLogin()
+    {
+
+
+
+        $this->assertTrue(false);
+    }
+}
