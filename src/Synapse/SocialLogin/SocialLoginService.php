762c18ea2e583486a2c073581a8e666d39907141
Aaron Bruce <aaron@synapsestudios.com>
2014-08-06 13:22:39 -0700

    Refs #67 - added update token logic

--- src/Synapse/SocialLogin/SocialLoginService.php
+++ src/Synapse/SocialLogin/SocialLoginService.php
@@ -52,7 +52,7 @@ class SocialLoginService
             $user = $this->userService->findById(
                 $socialLogin->getUserId()
             );
-
+            $this->updateSocialLoginToken($socialLogin, $request);
             return $this->handleLogin($user, $request);
         }
 
@@ -200,13 +200,20 @@ class SocialLoginService
     }
 
     /**
-     * Update a users OAuth information when they log in
+     * Update a users OAuth token, token expiration and refresh token information
+     * when they log in
      * 
-     * @param  UserEntity
+     * @param  SocialLoginEntity
      * @param  LoginRequest $request
      */
-    protected function updateFromSocialLogin(UserEntity $user, LoginRequest $request)
-    {
+    protected function updateSocialLoginToken(
+        SocialLoginEntity $socialLogin, 
+        LoginRequest $request
+    ) {
+        $socialLogin->setAccessToken($request->getAccessToken())
+            ->setAccessTokenExpires($request->getAccessTokenExpires())
+            ->setRefreshToken($request->getRefreshToken());
 
+        $this->socialLoginMapper->persist($socialLogin);
     }
 }
