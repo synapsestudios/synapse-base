e0ec5d9d487f75cd681258126b919945133a8a08
Chris Shiplet <chris.shiplet@synapsestudios.com>
2014-08-06 17:24:33 -0700

    Refs #94 - Fix improper indentation

--- src/Synapse/Install/RunInstallCommand.php
+++ src/Synapse/Install/RunInstallCommand.php
@@ -157,11 +157,11 @@ class RunInstallCommand extends AbstractDatabaseCommand
         $dropTables = $input->getOption('drop-tables');
 
         if (! $this->hasTables() || $dropTables) {
-        	// Throw an error if dropTables is set in production env
-        	if ($dropTables && $this->appEnv === 'production') {
-        		$output->writeln('Cannot drop tables when app environment is production.');
+            // Throw an error if dropTables is set in production env
+            if ($dropTables && $this->appEnv === 'production') {
+                $output->writeln('Cannot drop tables when app environment is production.');
                 return;
-        	}
+            }
 
             // Console message heading padded by a newline
             $output->write(['', '  -- APP INSTALL --', ''], true);
