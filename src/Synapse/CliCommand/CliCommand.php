76bdf54263c7d23ddaf71a954dd364b473269758
Andrew <andrew@andrew-NV79.(none)>
2014-08-22 22:51:37 -0700

    #112 | creating CliCommandArguments and CliCommand

---
+++ src/Synapse/CliCommand/CliCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Synapse\CliCommand;
+
+use Synapse\Arr;
+
+class CliCommand extends AbstractCliCommand
+{
+    protected $arguments;
+    protected $command;
+
+    public function __construct($command = null, CliCommandArguments $arguments = null)
+    {
+        $this->arguments = $arguments ?: new CliCommandArguments;
+        $this->command   = $command;
+    }
+
+    protected function getBaseCommand()
+    {
+        return trim(sprintf('%s %s', $this->command, $this->arguments->render()));
+    }
+
+    public function setCommand($command, CliCommandArguments $arguments = null)
+    {
+        $this->arguments = $arguments ?: new CliCommandArguments;
+        $this->command   = $command;
+    }
+}
