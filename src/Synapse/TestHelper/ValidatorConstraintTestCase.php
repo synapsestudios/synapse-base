eb217c7c2a06e003c6481646a0ba86a519650ea3
Paul Statezny <paul@synapsestudios.com>
2014-08-26 16:50:31 -0700

    Refs #113 - Create initial RowExists validator test.

---
+++ src/Synapse/TestHelper/ValidatorConstraintTestCase.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Synapse\TestHelper;
+
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ValidatorConstraintTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Captured constraint violations added by the validator
+     *
+     * @var array[
+     *      message
+     *      params
+     *      invalidValue
+     *      plural
+     *      code
+     * ]
+     */
+    protected $violations = [];
+
+    public function setUp(ConstraintValidator $validator)
+    {
+        $this->setUpMockExecutionContext();
+        $this->setUpMockConstraint();
+
+        $validator->initialize($this->mockExecutionContext);
+    }
+
+    /**
+     * Set up a mock execution context which captures violations
+     */
+    public function setUpMockExecutionContext()
+    {
+        $this->mockExecutionContext = $this->getMock('Symfony\Component\Validator\ExecutionContextInterface');
+
+        $callback = function ($message, array $params = [], $invalidValue = null, $plural = null, $code = null) {
+            $this->violations[] = [
+                'message'      => $message,
+                'params'       => $params,
+                'invalidValue' => $invalidValue,
+                'plural'       => $plural,
+                'code'         => $code,
+            ];
+        };
+
+        $this->mockExecutionContext->expects($this->any())
+            ->method('addViolation')
+            ->will($this->returnCallback($callback));
+    }
+
+    public function setUpMockConstraint()
+    {
+        $this->mockConstraint = $this->getMock('Symfony\Component\Validator\Constraint');
+    }
+
+    public function assertNoViolationsAdded()
+    {
+        $this->assertEmpty($this->violations);
+    }
+
+    public function assertViolationAdded($message, array $params = [], $value = null, $plural = null, $code = null)
+    {
+        $violation = [
+            'message'      => $message,
+            'params'       => $params,
+            'invalidValue' => $value,
+            'plural'       => $plural,
+            'code'         => $code,
+        ];
+
+        $this->assertContains($violation, $this->violations);
+    }
+}
---
+++ tests/Test/Synapse/Validator/Constraints/RowExistsValidatorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Test\Synapse\Validator\Constraints;
+
+use Synapse\TestHelper\ValidatorConstraintTestCase;
+use Synapse\Validator\Constraints\RowExistsValidator;
+use Test\Synapse\Entity\GenericEntity;
+
+class RowExistsValidatorTest extends ValidatorConstraintTestCase
+{
+    public function setUp()
+    {
+        $this->validator = new RowExistsValidator;
+
+        parent::setUp($this->validator);
+
+        $this->setUpMapperInMockConstraint();
+    }
+
+    public function setUpMockConstraint()
+    {
+        $this->mockConstraint = $this->getMockBuilder('Synapse\Validator\Constraints\RowExists')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function setUpMapperInMockConstraint()
+    {
+        $this->mockMapper = $this->getMockBuilder('Test\Synapse\Mapper\Mapper')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockConstraint->expects($this->any())
+            ->method('getMapper')
+            ->will($this->returnValue($this->mockMapper));
+    }
+
+    public function withEntityFound()
+    {
+        $entity = new GenericEntity;
+
+        $this->mockMapper->expects($this->any())
+            ->method('findBy')
+            ->will($this->returnValue($entity));
+    }
+
+    public function validateWithValue($value)
+    {
+        return $this->validator->validate($value, $this->mockConstraint);
+    }
+
+    public function testValidateAddsNoViolationsIfEntityFound()
+    {
+        $this->withEntityFound();
+
+        $this->validateWithValue('foo');
+
+        $this->assertNoViolationsAdded();
+    }
+}
