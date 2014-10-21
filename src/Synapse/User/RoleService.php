b3e3db0d17237de20cfa5a365b5822d34013c1f9
tdboone <tdboone@gmail.com>
2014-10-21 14:17:14 -0700

    #130 - added method to add role to a given user

---
+++ src/Synapse/User/RoleService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Synapse\User;
+
+/**
+ * Service for tasks relating to user roles
+ */
+class RoleService {
+
+    /**
+     * @var UserRolePivotMapper
+     */
+    protected $userRolePivotMapper;
+
+    /**
+     * @param UserRolePivotMapper $userRolePivotMapper
+     */
+    public function __construct(
+        UserRolePivotMapper $userRolePivotMapper
+    ) {
+        $this->userRolePivotMapper = $userRolePivotMapper;
+    }
+
+    /**
+     * Add a list of roles to a given user
+     *
+     * @param UserEntity $user
+     * @param array      $roles an array of strings
+     */
+    public function addRolesForUser(UserEntity $user, array $roles)
+    {
+        foreach ($roles as $role) {
+            $this->userRolePivotMapper->addRoleForUser($user->getId(), $role);
+        }
+    }
+}
--- src/Synapse/User/UserRolePivotMapper.php
+++ src/Synapse/User/UserRolePivotMapper.php
@@ -4,6 +4,7 @@ namespace Synapse\User;
 
 use Synapse\Mapper;
 use Synapse\Stdlib\Arr;
+use Zend\Db\Sql\Expression;
 
 /**
  * Mapper for user roles pivot table
@@ -34,5 +35,28 @@ class UserRolePivotMapper extends Mapper\AbstractMapper implements RoleFinderInt
         $results = $this->execute($query)->toArray();
 
         return Arr::pluck($results, 'name');
+    }
+
+    /**
+     * Add a given role to the given user id
+     *
+     * @param int    $userId
+     * @param string $roleName
+     */
+    public function addRoleForUser($userId, $roleName)
+    {
+        $roleIdExpression = new Expression(
+            '(SELECT `id` from `user_roles` WHERE `name` = ?)',
+            $roleName
+        );
+
+        $query = $this->getSqlObject()
+            ->insert()
+            ->values([
+                'user_id' => $userId,
+                'role_id' => $roleIdExpression,
+            ]);
+
+            $this->execute($query);
     }
 }
