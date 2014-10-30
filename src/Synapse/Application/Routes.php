65a0c17c798aca83a2dcd176f0ffd236643d12a0
Paul Statezny <paulstatezny@users.noreply.github.com>
2014-10-30 09:05:33 -0700

    Refs #133 - Catch 501 and 404 exceptions thrown by Silex.
    Format all responses in JSON.

--- src/Synapse/Application/Routes.php
+++ src/Synapse/Application/Routes.php
@@ -2,7 +2,6 @@
 
 namespace Synapse\Application;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Synapse\Application;
 
@@ -13,29 +12,49 @@ class Routes implements RoutesInterface
 {
     /**
      * {@inheritDoc}
+     *
+     * Has entries for both Synapse's and Symfony's 501 exceptions so that both return the same response
+     *
      * @param  Application $app
      */
     public function define(Application $app)
     {
-        $app->error(function (\Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException $e, $code) {
-            $body = ['error' => 'Access denied'];
-            return new JsonResponse($body, 403);
+        $routes = $this;
+
+        $app->error(function (\Synapse\Rest\Exception\MethodNotImplementedException $e, $code) use ($routes) {
+            return $routes->getMethodNotImplementedResponse();
         });
 
-        $app->error(function (\Synapse\Rest\Exception\MethodNotImplementedException $e, $code) {
-            $response = new Response('Method not implemented');
-            $response->setStatusCode(501);
-            return $response;
+        $app->error(function (\Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException $e, $code) use ($routes) {
+            return $routes->getMethodNotImplementedResponse();
+        });
+
+        $app->error(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $code) {
+            return new JsonResponse(['message' => 'Not found'], 404);
+        });
+
+        $app->error(function (\Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException $e, $code) {
+            return new JsonResponse(['message' => 'Access denied'], 403);
         });
 
         $app->error(function (\Exception $e, $code) use ($app) {
             $app['log']->addError($e->getMessage(), ['exception' => $e]);
 
-            if ($app['debug'] === false) {
-                return new Response('Something went wrong with your request');
-            } else {
+            if ($app['debug'] === true) {
                 throw $e;
             }
+
+            return new JsonResponse(['message' => 'Something went wrong with your request'], 500);
         });
+    }
+
+    /**
+     * Return a JSON 501 response
+     *
+     * @return JsonResponse
+     */
+    protected function getMethodNotImplementedResponse()
+    {
+        return new JsonResponse(['message' => 'Method not implemented'], 501);
     }
 }
--- src/Synapse/Security/Firewall/OAuth2Listener.php
+++ src/Synapse/Security/Firewall/OAuth2Listener.php
@@ -2,7 +2,7 @@
 
 namespace Synapse\Security\Firewall;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -68,8 +68,6 @@ class OAuth2Listener implements ListenerInterface
      */
     protected function getForbiddenReponse()
     {
-        $response = new Response();
-        $response->setStatusCode(401);
-        return $response;
+        return new JsonResponse(['message' => 'Unauthorized'], 401);
     }
 }
