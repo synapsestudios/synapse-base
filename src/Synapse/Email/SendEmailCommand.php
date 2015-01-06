fc7356c13a4b222666c086bd20ad656ccc448fef
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-01-06 13:14:09 -0700

    Refs #152 - Remove unnecessary use statement.

--- src/Synapse/Email/SendEmailCommand.php
+++ src/Synapse/Email/SendEmailCommand.php
@@ -2,7 +2,6 @@
 
 namespace Synapse\Email;
 
-use Synapse\Command\CommandInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
