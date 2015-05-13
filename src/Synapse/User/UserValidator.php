2da7cb74a987dd3121b34974a3c31ad613ff7d78
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-05-13 09:14:13 -0700

    Refs #213 - Check validity of current password in validator.

--- src/Synapse/User/UserValidator.php
+++ src/Synapse/User/UserValidator.php
@@ -3,18 +3,22 @@
 namespace Synapse\User;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ExecutionContextInterface as ExecutionContext;
 use Synapse\Entity\AbstractEntity;
 use Synapse\Validator\AbstractArrayValidator;
 
 class UserValidator extends AbstractArrayValidator
 {
     /**
+     * $contextEntity is the currently logged in user's UserEntity
+     *
      * {@inheritDoc}
      */
     protected function getConstraints(array $contextData, AbstractEntity $contextEntity = null)
     {
         $constraints = [
             'email' => new Assert\Optional([
+                new Assert\NotBlank(),
                 new Assert\Email([
                     'checkHost' => true
                 ]),
@@ -25,9 +29,27 @@ class UserValidator extends AbstractArrayValidator
         ];
 
         if (isset($contextData['email']) or isset($contextData['password'])) {
-            $constraints['current_password'] = new Assert\NotBlank();
+            $constraints['current_password'] = [
+                new Assert\NotBlank(),
+                new Assert\Callback([$this, 'validateCurrentPassword']),
+            ];
         }
 
         return $constraints;
+    }
+
+    /**
+     * Validate whether the current password is correct (meant to be used in a Callback constraint)
+     *
+     * Adds violation to $context if it's wrong
+     *
+     * @param  string           $password The current password
+     * @param  ExecutionContext $context
+     */
+    protected function validateCurrentPassword($password, ExecutionContext $context)
+    {
+        if (! password_verify($password, $contextEntity->getPassword())) {
+            $context->addViolation('INVALID');
+        }
     }
 }
